21e23e62c30ae51f4d0283f25649c21797982777
Shevanio <shevanio@localhost.localdomain>
2024-06-27 20:12:29 +0200

    auto get red

--- hostScan.sh
+++ hostScan.sh
@@ -9,13 +9,13 @@ function ctrl_c(){
 trap ctrl_c SIGINT
 
 tput civis
+IP=$(hostname -I | awk '{print $1}')
+OCTETS=$(echo $IP | cut -d '.' -f 1-3)
+
 
 echo "Active hosts:"
-for i in $(seq 1 254); do
-    #for port in 21 22 23 25 80 139 443 445 8080; do
-        #timeout 1 bash -c "echo '' > /dev/tcp/192.168.1.$i/$port" 2>/dev/null && echo "[+] Host 192.168.1.$i - Port $port (OPEN)" &
-    timeout 1 bash -c "ping -c 1 192.168.1.$i" &>/dev/null && echo "[+] Host 192.168.1.$i - Active" &
-    #done
+for lastoctetip in $(seq 1 254); do
+    timeout 1 bash -c "ping -c 1 $OCTETS.$lastoctetip" &>/dev/null && echo "[+] Host 192.168.1.$lastoctetip - Active" &
 done
 wait
 
@@ -23,11 +23,10 @@ echo
 
 echo "Ports discovered:"
 #declare -a ports=( $(seq 1 65535) )
-for i in $(seq 1 254); do
+for lastoctetip in $(seq 1 254); do
     for port in 21 22 23 25 80 139 443 445 8080; do
     #for port in ${ports[@]}; do
-        timeout 1 bash -c "echo '' > /dev/tcp/192.168.1.$i/$port" 2>/dev/null && echo "[+] Host 192.168.1.$i - Port $port (OPEN)" &
-        #timeout 1 bash -c "ping -c 1 192.168.1.$i" &>/dev/null && echo "[+] Host 192.168.1.$i - Active" &
+        timeout 1 bash -c "echo '' > /dev/tcp/$OCTETS.$lastoctetip/$port" 2>/dev/null && echo "[+] Host 192.168.1.$lastoctetip - Port $port (OPEN)" &
     done
 done
 
